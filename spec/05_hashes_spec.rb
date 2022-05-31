5ccd045845568078ef56f9744654451e6e7efbfb
Yasmin Morshed <snowmoss10@gmail.com>
2022-05-31 15:17:53 +0100

    Trying to figure out the fat arrow problem

--- spec/05_hashes_spec.rb
+++ spec/05_hashes_spec.rb
@@ -4,22 +4,47 @@ require "rspec"
 
 RSpec.describe "Hashes: " do
 
+    # it "We sometimes need a place to store our Valuables" do
+    #     # Create a hash
+    #     valuables = {
+    #         :gold => "mucho",
+    #         :purse => "not mucho",
+    #         :wallet => "full"
+    #     }
+    #     expect(valuables).to be_a(___)
+    # end
+
+    # it "we can then create a hash to store these valuable things" do
+    #     treasures = Hash.new('precious')
+    #     treasures[:gem] # => precious
+    #     treasures[:gold] = 'expensive' 
+    #     expect(treasures[:gem]).to eq('___')
+    #     expect(treasures[:gold]).to eq('___')
+    # end
+
     it "Use the key 🗝️ to find your value..." do
-        ages = {"Bob": 40, "Sally": 30, "Jim": 50}
-        expect(ages["Bob"]).to eq(___)
-        expect(ages[:Sally]).to eq(___)
+        ages = {'Bob': 40, 'Sally': 30, 'Jim': 50}
+        expect(ages[:Sally]).to eq(30)
     end
 
-    it "The arrows can also guide you to your value..." do
-        jobs = { "Jane" => "Artist", "Joe" => "Writer" }
-        expect(jobs.key("Writer")).to eq(___)
-    end
+    # it "The arrows can also guide you to your value..." do
+    #     jobs = { "Jane" => "Artist", "Joe" => "Writer" }
+    #     expect(jobs.key("Writer")).to eq(___)
+    # end
 
-    it "You sometimes want to modify your hash" do
-      jobs = { "Jane" => "Artist", "Joe" => "Writer" }
-      jobs["Jane"] = "Coding Guru"
-      expect(jobs["Jane"]).to eq(___)
-    end
+    # it "You sometimes want to modify your hash" do
+    #   jobs = { "Jane" => "Artist", "Joe" => "Writer" }
+    #   jobs["Jane"] = "Coding Guru"
+    #   expect(jobs["Jane"]).to eq(___)
+    # end
 
+end
 # Stuff to add:
 # Hashes in ruby
+
+# adding
+# removing
+# accessing
+# updating
+# iterating ??
+# 
