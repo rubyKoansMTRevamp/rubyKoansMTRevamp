b8f6755deaa0279d83df3ab98bf15180ab41b78e
Yasmin Morshed <snowmoss10@gmail.com>
2022-06-13 15:01:33 +0100

    started methods

--- spec/07_methods_spec.rb
+++ spec/07_methods_spec.rb
@@ -1,12 +1,22 @@
 require "rspec"
 
-# “Every one's got to find out for themselves what method suits them best.”
+# “Every one's got to find out for themselves what method suits them best.”...
 # ― Agatha Christie
 
 RSpec.describe "Methods" do
 
-    it "" do
-        expect().to eq(___)
+    def my_first_method(name)
+        return "Howdy, #{name}!"
+    end
+
+    def test_my_first_method_without_parentheses
+        result = my_first_method "Bob"
+        assert_equal my_first_method("Bob"), result
+      end
+
+    it "test first method" do
+        output = my_first_method("josh")
+        expect(greet).to eq(___)
     end
 
 end
