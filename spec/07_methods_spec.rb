c3d22941e6b7aea0689e912c8680fbcf79640420
Yasmin Morshed <snowmoss10@gmail.com>
2022-06-13 17:02:09 +0100

    Added some things to the methods file

--- spec/07_methods_spec.rb
+++ spec/07_methods_spec.rb
@@ -4,7 +4,7 @@ require "rspec"
 # ― Agatha Christie
 
 =begin
-Method is a collection of statements that perform some specific task and return the result. 
+Method is a collection of statements that perform some specific task and return the result!
 Methods are time savers and help the user to reuse the code without retyping the code. 
 Defining & Calling the method: 
 In Ruby, the method defines with the help of def keyword followed by method_name and end with end keyword.
@@ -12,41 +12,60 @@ For more info, have a look @: https://www.geeksforgeeks.org/ruby-methods/
 =end
 
 RSpec.describe "Methods" do
+
+    def give_me_coffee
+       " Coffee please."
+    end
+
+    it "gives task to ask for a coffee" do
+        expect(give_me_coffee).to eq(___)
+    end
+
+    it "request to have 1 cup of coffee using the give_me_coffee method" do
+        expect("Give me 1 cup of" + ___).to eq("Give me 1 cup of Coffee please.")
+    end
     
-    # def greeting(name)
-    #     return "Howdy, #{name}!"
-    # end
+    def greeting(name)
+        return "Howdy, #{name}!"
+        # We call this string interpolation.
+    end
 
-    # it "Introduction always comes first!" do
-    #     output = greeting("Josh")
-    #     expect(output).to eq("___")
-    # end
+    it "Greet yourself using your name as an argument!" do
+        output = greeting("___")
+        expect(output).to eq("Howdy, ___!")
+    end
 
-    # def addition(a, b)
-    #     return a + b
-    # end
+    def addition(a, b)
+        return a + b
+    end
 
-    # it "Let's add up some numbers!" do
-    #     sum = addition(10, 90)
-    #     expect(sum).to eq(___)
-    # end
+    it "Let's add up some numbers!" do
+        sum = addition(10, 90)
+        expect(sum).to eq(___)
+    end
 
     def multiply(a, b)
         a * b
     end
 
-    # it "Single line functions are cool, but mind the comma!" do
-    #    expect(6).to eq(multiply(2, ___))
-    # end
+    it "Single line functions are cool, but mind the comma!" do
+       expect(6).to eq(multiply(2, ___))
+    end
 
-    # Did you know, you can also use your methods without parentheses.
     def multiply a, b
         a * b
     end
 
-    it "You can use your methods without parentheses. Simple, huh?" do
-        expect(6).to eq(multiply(2, ___))
+    it "You can also use your methods without (), but it's best to keep them to make things easier to read?" do
+        expect(6).to eq(multiply 2, ___)
      end
  
+=begin
+Calling a method inside another method
+An example of iterating (maybe!)
+Passing in an array
+Mention that the last statement in ruby is implicitly returned (unless you have an explicit return?)
+Show an example of a default value for an argument, show an example of wrong number of arguments
+=end
 
 end
