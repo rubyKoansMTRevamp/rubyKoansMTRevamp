6d9519057df5c9fe18560c524b9b5033cb47e0e9
Yasmin Morshed <snowmoss10@gmail.com>
2022-06-13 16:08:52 +0100

    added some stuff to methods

--- spec/07_methods_spec.rb
+++ spec/07_methods_spec.rb
@@ -1,22 +1,52 @@
 require "rspec"
 
-# “Every one's got to find out for themselves what method suits them best.”...
+# “Every one's got to find out for themselves what method suits them best!”
 # ― Agatha Christie
 
+=begin
+Method is a collection of statements that perform some specific task and return the result. 
+Methods are time savers and help the user to reuse the code without retyping the code. 
+Defining & Calling the method: 
+In Ruby, the method defines with the help of def keyword followed by method_name and end with end keyword.
+For more info, have a look @: https://www.geeksforgeeks.org/ruby-methods/
+=end
+
 RSpec.describe "Methods" do
+    
+    # def greeting(name)
+    #     return "Howdy, #{name}!"
+    # end
 
-    def my_first_method(name)
-        return "Howdy, #{name}!"
+    # it "Introduction always comes first!" do
+    #     output = greeting("Josh")
+    #     expect(output).to eq("___")
+    # end
+
+    # def addition(a, b)
+    #     return a + b
+    # end
+
+    # it "Let's add up some numbers!" do
+    #     sum = addition(10, 90)
+    #     expect(sum).to eq(___)
+    # end
+
+    def multiply(a, b)
+        a * b
     end
 
-    def test_my_first_method_without_parentheses
-        result = my_first_method "Bob"
-        assert_equal my_first_method("Bob"), result
-      end
+    # it "Single line functions are cool, but mind the comma!" do
+    #    expect(6).to eq(multiply(2, ___))
+    # end
 
-    it "test first method" do
-        output = my_first_method("josh")
-        expect(greet).to eq(___)
+    # Did you know, you can also use your methods without parentheses.
+    def multiply a, b
+        a * b
     end
+
+    it "You can use your methods without parentheses. Simple, huh?" do
+        expect(6).to eq(multiply(2, ___))
+     end
+ 
 
 end
